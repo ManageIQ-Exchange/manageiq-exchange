c5935b618d68e2a7cf1f00a9be5491d8cf5c71d1
Sergio Ocon <sergio.ocon@redhat.com>
2017-11-24 13:28:46 +0100

    adding api versioning and version 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Devise
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.devise_mailer_host,
+                                               port: Rails.application.secrets.devise_mailer_port }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get 'api/version'
+  devise_for :users
 
+
+  concern :api_base  do
+    get 'api/version'
+  end
+
+
+  namespace :v1 do
+    concerns :api_base
+  end
+
+  scope module: 'v1', path: 'latest' do
+    concerns :api_base
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "v1#version", as: 'root'
 end
