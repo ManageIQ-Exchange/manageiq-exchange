615147db969cc65ff9faa993ada0f51f48a56f83
Sergio Ocon <sergio.ocon@redhat.com>
2017-11-28 20:15:25 +0100

    authentication

--- app/controllers/v1/github_controller.rb
+++ app/controllers/v1/github_controller.rb
@@ -1,11 +1,21 @@
 module V1
   class GithubController < ApplicationController
     def access_token
-      access_token = params[:code] ? github_access.exchange_code_for_token(params[:code]) : nil
-      if access_token[:access_token]
+      code = params[:code] || request.headers[:code]
+      begin
+        access_token = code ? github_access.exchange_code_for_token(code) : nil
+      rescue Octokit::NotFound
+        render json: { error: { message: "Invalid code"} }, status: :fail
+        return
+      end
+      if access_token.try(:access_token)
         render json: { token: access_token[:access_token] }, status: :ok
       else
-        render json: { error: { message: "Need a post param code" }}, status: :fail unless params[:code]
+        if access_token[:error]
+          render json: access_token, status: :fail
+        else
+          render json: { error: { message: "Need a post param code" }}, status: :fail
+        end
       end
     end
 
@@ -17,7 +27,7 @@ module V1
     private
 
     def github_access
-      @github_access ||= Octokit::Client.new client_id: ENV["GITHUB_OAUTH_ID"], client_secret: ENV["GITHUB_OAUTH_SECRET"], :scope => 'user:email'
+      @github_access ||= Octokit::Client.new client_id: Rails.application.secrets.oauth_github_id, client_secret: Rails.application.secrets.oauth_github_secret, :scope => 'user:email'
     end
 
     def github_token(token)
