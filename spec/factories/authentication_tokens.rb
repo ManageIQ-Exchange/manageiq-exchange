7979809ce6ca01aea174a3eada40702041992de1
Sergio Ocon <sergio.ocon@redhat.com>
2017-12-15 14:55:33 +0100

    new factories

--- spec/factories/authentication_tokens.rb
+++ spec/factories/authentication_tokens.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :authentication_token do
-    body 'MyString'
-    user nil
-    last_used_at '2017-11-29 14:32:14'
-    ip_address 'MyString'
-    user_agent 'MyString'
+    body { Faker::Crypto.md5 }
+    user
+    last_used_at { Faker::Time.between(DateTime.now - 2.years, DateTime.now) }
+    ip_address { Faker::Internet.ip_v4_address }
+    user_agent { Faker::Internet.user_agent }
+    github_token { Faker::Crypto.md5 }
   end
 end
--- spec/factories/spins.rb
+++ spec/factories/spins.rb
@@ -1,4 +1,39 @@
 FactoryBot.define do
   factory :spin do
+    id                { Faker::Number.number(10) }
+    published         false
+    user
+    name              { Faker::App.name.parameterize }
+    full_name         { "#{user.github_login}/#{name}" }
+    description       { Faker::Lorem.sentence }
+    clone_url         { Faker::Internet.url }
+    forks_count       { Faker::Number.between(0, 10) }
+    stargazers_count  { Faker::Number.between(0, 10) }
+    watchers_count    { Faker::Number.between(0, 10) }
+    open_issues_count { Faker::Number.between(0, 10) }
+    size              { Faker::Number.between(0, 10) }
+    gh_id             { id }
+    gh_created_at     { Faker::Time.backward(100) }
+    gh_pushed_at      { Faker::Time.between(gh_created_at, Date.today) }
+    gh_updated_at     { Faker::Time.between(gh_created_at, Date.today) }
+    gh_archived       false
+    default_branch    'master'
+    license_key       "MIT"
+    license_name      "MIT License"
+    license_html_url  "https://api.github.com/licenses/mit"
+    version           { "0.#{Faker::Number.between(0,10)}.#{Faker::Number.between(0,10)}" }  # Should be Faker::App.semantic_version when it works
+    metadata          do
+      { "tags":[], "author": user.name, "company": nil, "license": license_key, "dependencies": nil, "spin_version": version, "min_miq_version"=>"h"}.to_json
+    end
+    min_miq_version   { Faker::Number.between(5, 9) }
+    first_import      { Faker::Time.between(gh_created_at, Date.today) }
+    score             0
+    company           { Faker::Company.name }
+    readme            { Faker::Lorem.sentence }
+
+    trait :published do
+      published true
+    end
+
   end
 end
