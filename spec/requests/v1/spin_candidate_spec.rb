c4844b437a86e48b55dca09092a4ad55a1a868cf
Sergio Ocon <sergio.ocon@redhat.com>
2018-02-06 11:50:12 +0100

    tested 401

--- spec/requests/v1/spin_candidate_spec.rb
+++ spec/requests/v1/spin_candidate_spec.rb
@@ -1,15 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'V1::SpinCandidates', type: :request do
-  context "when not authenticated " do
-    VCR.use_cassette('spins/spin-candidates-bad-auth') do
-      pending '#index returns 401'
-      pending '#show returns 401'
-      pending '#refresh returns 401'
+  let!(:prefix) { 'v1' }
+
+  context 'when not authenticated' do
+    it '#index returns 401' do
+      get "/#{prefix}/spin_candidates"
+      expect(response).to have_http_status(401)
+    end
+
+    it '#show returns 401' do
+      get "/#{prefix}/spin_candidates/1"
+      expect(response).to have_http_status(401)
+    end
+
+    it '#publish returns 401' do
+      post "/#{prefix}/spin_candidates/1/publish"
+      expect(response).to have_http_status(401)
+    end
+
+    it '#refresh returns 401' do
+      post "/#{prefix}/spin_candidates/refresh"
+      expect(response).to have_http_status(401)
     end
   end
-  context "when authenticated" do
-    pending "index"
+  context 'when authenticated' do
+    pending "index" 
     pending "show when valid id"
     pending "show when invalid id"
     pending "refresh when same repos"
