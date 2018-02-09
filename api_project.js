2036bc1fa36a190627d9ecd19b37ac3d0a2ff1d5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-09 12:06:23 +0000

    Deploy ManageIQ-Exchange/manageiq-exchange to github.com/ManageIQ-Exchange/manageiq-exchange.git:gh-pages

--- api_project.js
+++ api_project.js
@@ -1,1 +1,16 @@
-define({  "name": "ManageIQ Exchange",  "version": "1.0.0",  "description": "Exchange API doc",  "title": "ManageIQ Exchange API",  "url": "https://exchange-backend.manageiq.org/v1",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-02-08T19:18:15.838Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
+define({
+  "name": "ManageIQ Exchange",
+  "version": "1.0.0",
+  "description": "Exchange API doc",
+  "title": "ManageIQ Exchange API",
+  "url": "https://exchange-backend.manageiq.org/v1",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-02-09T12:06:16.876Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
