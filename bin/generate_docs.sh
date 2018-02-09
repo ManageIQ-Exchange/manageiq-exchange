139065bb0c269bf795b7814a90cc4bafc0afc3c3
Alberto Gutierrez <agutierr@redhat.com>
2018-02-09 12:14:00 +0100

    Travis (#105)

--- bin/generate_docs.sh
+++ bin/generate_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 INPUT=app
-OUTPUT=docs
+OUTPUT=gh_pages
 MY_PATH=`dirname $0`              # relative
 MY_PATH=$( cd $MY_PATH/.. && pwd ) 
 echo Generating documentation outpout in $MY_PATH/$OUTPUT
