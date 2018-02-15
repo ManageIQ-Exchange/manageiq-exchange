4eda61b99471e0f23bacd79ad139277d2533c06e
Alberto Jesus Gutierrez Juanes <aljesusg@gmail.com>
2018-02-15 10:07:10 +0100

    Fix search users

--- app/controllers/v1/spins_controller.rb
+++ app/controllers/v1/spins_controller.rb
@@ -15,8 +15,9 @@ module V1
     # spins?query=<value>  Look spins include value in the name
     def index
       @spins = Spin.where(visible:true)
+      byebug
       @spins = @spins.where("'name' ILIKE ?", "%#{params[:name]}%") if params[:name]
-      @spins = @spins.joins(:user).where("'users.github_login' ILIKE ?", "%#{params[:author]}%") if params[:author]
+      @spins = @spins.joins(:user).where("users.github_login ILIKE ?", "%#{params[:author]}%") if params[:author]
       @spins = @spins.order("#{params[:sort]} #{params[:order] || 'DESC'}") if params[:sort]
       if @spins.count.positive?
         return_response @spins, :ok, {}
